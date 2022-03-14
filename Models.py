4412793e441ad9db7076a3e76c57340652bdee02
EduardiTorre <eduardozapera@gmail.com>
2022-03-14 15:20:39 -0300

    criacao models da mercearia

---
+++ Models.py
@@ -0,0 +1,40 @@
+from sre_constants import CATEGORY_UNI_NOT_LINEBREAK
+
+
+class Categorias:
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+class Produtos:
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+ 
+class Estoque:
+    def __init__(self, produto: Produtos, quantidade):
+        self.produto = produto
+        self.quantidade = quantidade
+
+class Fornecedor:
+    def __init__(self, nome, cnpj, telefone, categoria):
+        self.nome = nome 
+        self.cnpj = cnpj
+        self.telefone = telefone
+        self. categoria = categoria
+
+# cliente
+class Pessoa:
+    def __init__(self, nome, telefone, cpf, email, endereco):
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
+        self.endereco = endereco
+
+# herda cliente pois o funcionario tem os mesmos atributos
+class Funcionario(Pessoa): 
+    def __init__(self, clt, nome, telefone, cpf, email, endereco):
+        self.clt = clt
+        super(Funcionario, self).__init__(nome, telefone, cpf, email, endereco)
+
